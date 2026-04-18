171ec743ed1fa9edc24c9fd1084b9760b6615101
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-04-18 20:19:32 -0300

    feature: Criação de migration para criar a tabela "categories"

---
+++ backend/prisma/migrations/20260418231840_create_categories_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "icon" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "categories_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
