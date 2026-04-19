48be0815160467ee6f8a30731e62405a30c47de1
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-04-19 18:19:04 -0300

    refactor: Adicionando FieldResolvers para os campos "user" e "category" da transaction

--- backend/src/resolvers/transaction.resolver.ts
+++ backend/src/resolvers/transaction.resolver.ts
@@ -1,4 +1,12 @@
-import { Arg, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql'
+import {
+  Arg,
+  FieldResolver,
+  Mutation,
+  Query,
+  Resolver,
+  Root,
+  UseMiddleware
+} from 'type-graphql'
 import {
   CreateTransactionInput,
   UpdateTransactionInput
@@ -6,16 +14,24 @@ import {
 import type { User } from '@/generated/prisma/client.js'
 import { GqlUser } from '@/graphql/decorators/user.decorator.js'
 import { IsAuth } from '@/middlewares/auth.middleware.js'
+import { CategoryModel } from '@/models/category.model.js'
 import { TransactionModel } from '@/models/transaction.model.js'
+import { UserModel } from '@/models/user.model.js'
+import { CategoryService } from '@/services/category.service.js'
 import { TransactionService } from '@/services/transaction.service.js'
+import { UserService } from '@/services/user.service.js'
 
 @Resolver(() => TransactionModel)
 @UseMiddleware(IsAuth)
 export class TransactionResolver {
   private readonly transactionService: TransactionService
+  private readonly userService: UserService
+  private readonly categoryService: CategoryService
 
   constructor() {
     this.transactionService = new TransactionService()
+    this.userService = new UserService()
+    this.categoryService = new CategoryService()
   }
 
   @Mutation(() => TransactionModel)
@@ -55,5 +71,18 @@ export class TransactionResolver {
     await this.transactionService.delete(id, user.id)
 
     return true
+  }
+
+  @FieldResolver(() => UserModel)
+  async user(@Root() transaction: TransactionModel) {
+    return this.userService.getById(transaction.userId)
+  }
+
+  @FieldResolver(() => CategoryModel)
+  async category(@Root() transaction: TransactionModel) {
+    return this.categoryService.getById(
+      transaction.categoryId,
+      transaction.userId
+    )
   }
 }
