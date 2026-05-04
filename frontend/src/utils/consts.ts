38d2a844062f0fac00482b9cbad485f62c30fb73
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-05-04 19:53:55 -0300

    feat: criação do layout base da página de transações

--- frontend/src/utils/consts.ts
+++ frontend/src/utils/consts.ts
@@ -9,3 +9,51 @@ export const availableColors: CategoryColor[] = [
   'yellow',
   'green'
 ]
+
+export const colorVariants: Record<
+  CategoryColor,
+  { iconBg: string; iconText: string; tagBg: string; tagText: string }
+> = {
+  blue: {
+    iconBg: 'bg-blue-light',
+    iconText: 'text-blue-base',
+    tagBg: 'bg-blue-light',
+    tagText: 'text-blue-dark'
+  },
+  purple: {
+    iconBg: 'bg-purple-light',
+    iconText: 'text-purple-base',
+    tagBg: 'bg-purple-light',
+    tagText: 'text-purple-dark'
+  },
+  pink: {
+    iconBg: 'bg-pink-light',
+    iconText: 'text-pink-base',
+    tagBg: 'bg-pink-light',
+    tagText: 'text-pink-dark'
+  },
+  red: {
+    iconBg: 'bg-red-light',
+    iconText: 'text-red-base',
+    tagBg: 'bg-red-light',
+    tagText: 'text-red-dark'
+  },
+  orange: {
+    iconBg: 'bg-orange-light',
+    iconText: 'text-orange-base',
+    tagBg: 'bg-orange-light',
+    tagText: 'text-orange-dark'
+  },
+  yellow: {
+    iconBg: 'bg-yellow-light',
+    iconText: 'text-yellow-base',
+    tagBg: 'bg-yellow-light',
+    tagText: 'text-yellow-dark'
+  },
+  green: {
+    iconBg: 'bg-green-light',
+    iconText: 'text-green-base',
+    tagBg: 'bg-green-light',
+    tagText: 'text-green-dark'
+  }
+}
--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -24,3 +24,18 @@ export interface Category {
   createdAt: string
   updatedAt: string
 }
+
+export type TransactionType = 'INCOME' | 'EXPENSE'
+
+export interface Transaction {
+  id: string
+  description: string
+  date: string
+  category: {
+    name: string
+    color: CategoryColor
+    icon: string
+  }
+  type: TransactionType
+  amount: number
+}
